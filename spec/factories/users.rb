0be4589549392ec0e053199b3cd21344ae1414e8
Andrew N.Romanov <andrew.n.romanov@gmail.com>
2019-12-17 07:43:11 +0300

    WIP specs.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
     email { "MyString" }
-    password { "" }
+    password { "MyPassword" }
   end
 end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 RSpec.feature "Users", type: :feature do
+  let(:user) { create :user }
+
   scenario "GET /users" do
+    visit login_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Save'
+
     visit users_path
+
     expect(page).to have_content 'Users'
   end
 end
